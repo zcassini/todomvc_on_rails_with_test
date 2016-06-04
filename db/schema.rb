a6b5d9aeb9417db5fa3d8d31a002cbc181af5dbc
vagrant <zcassini@gmail.com>
2016-06-04 06:37:38 -0700

    switch local db from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130518030840) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.boolean  "completed",  default: false, null: false
     t.string   "title"
